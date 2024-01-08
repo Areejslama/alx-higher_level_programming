1cae825ff2e1c2e641da5b062be8d21819e92c4c
Areejslama <Areegahmed449@gmail.com>
2024-01-07 18:44:32 -0800

    r

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == []:
-        return 0, None
+        sentence is None
     else:
         return len(sentence), sentence[0]
+    return(0, None)
