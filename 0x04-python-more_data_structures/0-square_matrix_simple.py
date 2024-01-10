26e0d8addf9cc1886698f33402f219913edd9e25
Areejslama <Areegahmed449@gmail.com>
2024-01-10 09:06:29 -0800

    matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    squared_matrix = []
+    for i in matrix:
+        squared_raw = list(map(lambda row: row**2, i))
+        squared_matrix.append(squared_raw)
+        return squared_matrix
