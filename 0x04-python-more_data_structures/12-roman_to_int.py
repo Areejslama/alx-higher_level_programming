8a47bd26a121e1d411c6078a31d6cb05b0cafa0e
Areejslama <Areegahmed449@gmail.com>
2024-01-10 18:42:54 -0800

    roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    if type(roman_string) is not str or roman_string is None:
         return 0
 
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    int_value = 0
-    i = 0
+    roman_letters = [
+        ['M', 1000], ['D', 500], ['C', 100], ['L', 50],
+        ['X', 10], ['V', 5], ['I', 1]
+    ]
+    num = 0
+    last = 0
 
-    while i < len(roman_string):
-        if i + 1 < len(roman_string) and roman_dict[roman_string[i:i + 2]] > roman_dict[roman_string[i]]:
-            int_value -= roman_dict[roman_string[i:i + 2]]
-            i += 2
-        else:
-            int_value += roman_dict[roman_string[i]]
-            i += 1
+    for letter in roman_string:
+        for elem in roman_letters:
+            if letter == elem[0]:
+                if last == 0 or last >= elem[1]:
+                    num += elem[1]
+                elif last < elem[1]:
+                    num += elem[1] - (last * 2)
 
-    
-    if int_value < 1 or int_value > 3999:
-        return 0
+                last = elem[1]
 
-    return int_value
+    return num
