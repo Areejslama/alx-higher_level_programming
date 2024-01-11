9cf8a70ec6775a93fdcb05a2942ce1a761ae2b0c
Areejslama <Areegahmed449@gmail.com>
2024-01-10 18:13:26 -0800

    best

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if  not a_dictionary:
+    if not a_dictionary:
         return None
     new_dic = list(a_dictionary.keys())
     max_v = 0
-    l = ""
+    dic = ""
     for i in new_dic:
         if a_dictionary[i] > max_v:
             max_v = a_dictionary[i]
-            l = i
-    return l
+            dic = i
+    return dic
