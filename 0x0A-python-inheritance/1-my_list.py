71e228d35f27cc560e04d86fb00851c32e8cf58e
Areejslama <Areegahmed449@gmail.com>
2024-02-05 15:28:40 -0800

    list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""fuction to print list"""
+
+class MyList(list):
+    """present subclass"""
+    def print_sorted(self):
+        """print list"""
+        print(sorted(self))
