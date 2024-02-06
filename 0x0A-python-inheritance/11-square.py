9bb9a50200740b661e20c35391aae451be55dd16
Areejslama <Areegahmed449@gmail.com>
2024-02-06 15:08:00 -0800

    square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -4,43 +4,54 @@ Contains the class BaseGeometry and subclass Rectangle
 """
 
 
+
 class BaseGeometry:
-    """represent a geometry base class"""
+    """Base class for geometry"""
+
     def area(self):
-        raise NotImplementedError("area() is not implemented")
+        """Placeholder area method"""
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate if value is an integer greater than 0."""
-        if not isinstance(value, int):
+        """Validate the value as an integer greater than 0"""
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
-    """represent a rectangle"""
+    """Class representing a rectangle"""
+
     def __init__(self, width, height):
-        """instant of rectangle"""
+        """Initialize the rectangle"""
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
 
     def area(self):
+        """Calculate the area of the rectangle"""
         return self.__width * self.__height
 
     def __str__(self):
-        """represent a string"""
-        return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
+        """Return the string representation of the rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
 
 class Square(Rectangle):
-    """represent a square"""
+    """Class representing a square, inherits from Rectangle"""
+
     def __init__(self, size):
+        """Initialize the square"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
-        return self.__size * self.__size
+        """Calculate the area of the square"""
+        return self.__size ** 2
 
     def __str__(self):
+        """Return the string representation of the square"""
         return "[Square] {}/{}".format(self.__size, self.__size)
