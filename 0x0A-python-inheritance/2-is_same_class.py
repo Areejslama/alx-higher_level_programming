cce745fa274ef03e79430865471e477fe435ad05
Areejslama <Areegahmed449@gmail.com>
2024-02-05 16:17:24 -0800

    sub

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     """represent the subclass"""
-    if issubclass(a_class, int):
+    if issubclass(a_class, object):
         return True
     else:
         return False
