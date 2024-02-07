3382091ad48ac63d5b691ea81fbd430027cfa090
Areejslama <Areegahmed449@gmail.com>
2024-02-06 18:05:48 -0800

    100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,7 +5,9 @@
 class MyInt(int):
     def __init__(self, value):
         self.value = value
-    def __eq__(self, other):
-       return self.value != other.value
-    def __ne__(self, other):
-        return self.value == other.value
+
+        def __eq__(self, other):
+            return self.value != other.value
+
+        def __ne__(self, other):
+            return self.value == other.value
