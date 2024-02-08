123a017cd882ae33df915e50738713965bbbfdf2
Areejslama <Areegahmed449@gmail.com>
2024-02-08 14:06:46 -0800

    append

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""define a function"""
+
+
+def append_write(filename="", text=""):
+    """represent the function"""
+    with open(filename, "a", encoding="UTF8") as f:
+        return(f.write(text))
