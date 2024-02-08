14a64744be495fd721c85f960b42eec423764f96
Areejslama <Areegahmed449@gmail.com>
2024-02-08 07:40:08 -0800

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def read_file(filename="my_file_0.txt"):
+def read_file(filename=""):
     with open("my_file_0.txt", encoding="UTF8") as my_file:
         a_file = my_file.read()
-        print(a_file, end= "")
+        print(a_file, end="")
