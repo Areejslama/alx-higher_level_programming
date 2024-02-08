365bf59bf430f899df1c64c3ad4651cb2180c568
Areejslama <Areegahmed449@gmail.com>
2024-02-08 07:54:48 -0800

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,7 @@
 
 def read_file(filename=""):
     """represernt the function"""
-    with open("my_file_0.txt", "rt", encoding="UTF8") as my_file:
+
+    with open(filename, "rt", encoding="UTF8") as my_file:
         a_file = my_file.read()
         print(a_file, end="")
