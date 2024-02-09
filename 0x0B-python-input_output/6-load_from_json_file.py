b036f0ac847581323c309019dc897a3bbf2b4289
Areejslama <Areegahmed449@gmail.com>
2024-02-08 16:52:04 -0800

    obj

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""represent a function"""
+import json
+
+
+def load_from_json_file(filename):
+    """represent the function
+    Args:
+    filename:the name of file object
+    Return:
+    created object
+    """
+    with open(filename, "r") as f:
+        json.load(f)
