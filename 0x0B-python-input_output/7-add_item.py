7ca3818d52af438226d4b49a3d16a7019524600a
Areejslama <Areegahmed449@gmail.com>
2024-02-09 05:50:38 -0800

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """represent file"""
+
 import json
 import sys
+
 
 save_file = __import__('5-save_to_json_file').save_to_json_file
 load_file = __import__('6-load_from_json_file').load_from_json_file
