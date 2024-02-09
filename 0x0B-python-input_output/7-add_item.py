325921c24af155145657501fb140d9f940dec2ba
Areejslama <Areegahmed449@gmail.com>
2024-02-08 17:44:38 -0800

    add

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import json
+import sys
+
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+
+file = add_item.json
+
+try:
+    new = load(file)
+except(FileNotFoundError, ValueError):
+    new = []
+    for i in sys.argv[1:]:
+        new.append(i)
+        save(new, file)
+
+
