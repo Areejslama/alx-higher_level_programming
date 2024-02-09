d7169131ef490282ee0fa558360f293316105efa
Areejslama <Areegahmed449@gmail.com>
2024-02-09 09:14:43 -0800

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,18 +3,17 @@
 
 import json
 import sys
-
-
-load_file =  __import__('6-load_from_json_file').load_from_json_file
-save_file =  __import__('5-save_to_json_file').save_to_json_file
-
+save_file = __import__('5-save_to_json_file').save_to_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
 try:
-    my_file = load_file(filename)
+    data = load_file(filename)
 except (FileNotFoundError, ValueError):
-    my_file = []
-    for args in sys.argv[1:]:
-        my_file.append(args)
-        save_file(my_file, filename)
+    data = []
+
+for arg in sys.argv[1:]:
+    data.append(arg)
+
+save_file(data, filename)
