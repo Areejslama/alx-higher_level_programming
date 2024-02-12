5cc11032e8d8915cb63460f529257365d1fa7c0a
Areejslama <Areegahmed449@gmail.com>
2024-02-12 09:35:09 -0800

    15

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """define a base class"""
 
-from models.base import Base
+
+class Base:
+    """reprsent base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """init base class"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
 
 class Rectangle(Base):
