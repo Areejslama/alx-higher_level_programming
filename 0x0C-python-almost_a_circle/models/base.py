1bc932e23117e475214eedec83ea9d9ec3dd8668
Areejslama <Areegahmed449@gmail.com>
2024-02-12 05:10:29 -0800

    18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,25 +14,34 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
-    def to_json_string(list_dictionaries):
-        if list_dictionaries is None:
-            return "[]"
-        else:
-            return json.dumps(list_dictionaries)
-    
-    @classmethod
-    def save_to_file(cls, list_objs):
-        filename = cls.__name__ + ".json"
-        with open(filename, mode='w') as file:
-            if list_objs is None:
-                file.write("[]")
-            else:
-                list_dicts = [obj.to_dictionary() for obj in list_objs]
-                json_string = cls.to_json_string(list_dicts)
-                file.write(json_string)
-    def from_json_string(json_string):
-        if json_string is None:
-            return "[]"
-        else:
-            return json.loads(json_string)
+
+            def to_json_string(list_dictionaries):
+                if list_dictionaries is None:
+                    return "[]"
+                else:
+                    return json.dumps(list_dictionaries)
+
+                @classmethod
+                def save_to_file(cls, list_objs):
+                    filename = cls.__name__ + ".json"
+                    with open(filename, mode='w') as file:
+                        if list_objs is None:
+                            file.write("[]")
+                        else:
+                            for obj in list_objs:
+                                list_dicts = [obj.to_dictionary()]
+                                json_string = cls.to_json_string(list_dicts)
+                                file.write(json_string)
+
+                            def from_json_string(json_string):
+                                if json_string is None:
+                                    return "[]"
+                                else:
+                                    return json.loads(json_string)
+
+                                @classmethod
+                                def create(cls, **dictionary):
+                                    if cls.__name__ == "Rectangle":
+                                        dummy = cls(1, 1, 1)
+                                        dummy.update(**dictionary)
+                                        return dummy
