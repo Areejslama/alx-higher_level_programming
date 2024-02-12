c42a4809b273f3a1722f8d7cb6b4af068079c402
Areejslama <Areegahmed449@gmail.com>
2024-02-12 15:36:21 -0800

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -48,7 +48,10 @@ class Base:
     def create(cls, **dictionary):
         """Create instance from dictionary"""
         if cls.__name__ == "Rectangle":
-            dummy = cls(1, 1, 1)
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+                dummy = cls(1)
+        else:
             dummy.update(**dictionary)
             return dummy
 
