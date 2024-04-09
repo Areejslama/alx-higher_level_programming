43ea9455990f4657b88f5bab0f9fa5e467f7ba67
Areejslama <Areegahmed449@gmail.com>
2024-04-09 15:37:15 -0700

    8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const x = Number(process.argv[2]);
-
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
+	const x = Number(process.argv[2])
   for (let i = 0; i < x; i++) {
     console.log('x'.repeat(x));
   }
