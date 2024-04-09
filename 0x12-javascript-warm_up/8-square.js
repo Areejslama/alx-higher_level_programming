e09729732a2fb6ca91c53d953ecf5173f48648c3
Areejslama <Areegahmed449@gmail.com>
2024-04-09 15:20:27 -0700

    8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,13 +3,9 @@
 const str = parseInt(process.argv[2]);
 
 if (isNaN(str)) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let num = 0; num < str; num++) {
-        let row = '';
-        for (let i = 0; i < str; i++) {
-            row += 'x';
-        }
-        console.log(row);
-    }
+  for (let i = 0; i < str; i++) {
+    console.log('x'.repeat(str));
+  }
 }
