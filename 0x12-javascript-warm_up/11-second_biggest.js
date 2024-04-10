ea7e9634657a671246f3cb42b66c42a294e435e2
Areejslama <Areegahmed449@gmail.com>
2024-04-09 19:13:56 -0700

    12

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = parseInt(process.argv[2]);
+
+if (isNaN(args)) {
+    console.log(0);
+} else if (args === 0) {
+    console.log(0);
+} else if (args === 1) {
+    console.log(1);
+} else {
+	console.log(args); }
