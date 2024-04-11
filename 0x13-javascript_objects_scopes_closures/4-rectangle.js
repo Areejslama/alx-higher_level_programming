891e5c4ca2020e506a2395eae623b8634a03d6b1
Areejslama <Areegahmed449@gmail.com>
2024-04-11 08:25:30 -0700

    4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let n = 0; n < this.height; n++) {
+      let row = '';
+      for (let i = 0; i < this.width; i++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+  rotate() {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
