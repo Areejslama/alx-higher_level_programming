b4f44d20df9b2939ee6d3d70281b7b712a0acb02
Areejslama <Areegahmed449@gmail.com>
2024-04-11 18:29:03 -0700

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
 
-const args = parseInt(process.argv[2]);
-
-if (isNaN(args)) {
-  console.log(0);
-} else if (args === 0) {
-  console.log(0);
-} else if (args === 1) {
-  console.log(1);
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
-  console.log(args);
+  const list = process.argv.slice(2).map(Number);
+  const biggest = list.sort(function (a, b) { return b - a; })[1];
+  console.log(biggest);
 }
