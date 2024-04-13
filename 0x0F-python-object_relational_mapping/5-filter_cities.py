7c0dc83ea0b5df9d681294518861474448be8895
Areejslama <Areegahmed449@gmail.com>
2024-04-13 16:48:31 -0700

    4

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,9 @@ if __name__ == "__main__":
     city = sys.argv[4]
     cur.execute("""SELECT cities.id, cities.name FROM
                 cities JOIN states ON states.id=cities.state_id
-                WHERE states.name = %s""", (city,))
+                 WHERE states.name = %s
+                 ORDER BY cities.id ASC
+                 """, (city,))
     towns = cur.fetchall()
     for i in towns:
         print(i)
