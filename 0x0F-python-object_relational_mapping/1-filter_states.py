6124d2ca4503451333d1cc709ea4120b0594e69d
Areejslama <Areegahmed449@gmail.com>
2024-04-13 07:07:47 -0700

    1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""this script list states with name"""
+import sys
+import MySQLdb
+
+if  __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    city = cur.fetchall()
+    for i in city:
+        print(city)
