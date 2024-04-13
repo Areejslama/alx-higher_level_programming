144f8e15f55e859644c2c4ff4df5e99b56b146c0
Areejslama <Areegahmed449@gmail.com>
2024-04-13 14:11:21 -0700

    4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,7 +6,6 @@ import MySQLdb
 db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                      passwd=sys.argv[2], db=sys.argv[3], port=3306)
 cur = db.cursor()
-cur = db.cursor()
 cur.execute("SELECT * FROM cities ORDER BY id ASC")
 town = cur.fetchall()
 for i in town:
