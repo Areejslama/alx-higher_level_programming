f4304191526d9b24682346e04cb16fb85f5090f5
Areejslama <Areegahmed449@gmail.com>
2024-04-13 16:25:56 -0700

    5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     city = sys.argv[4]
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+    cur.execute("""SELECT cities.id, cities.name FROM
                 cities JOIN states ON states.id=cities.state_id
                 WHERE states.name LIKE BINARY %s""", (city,))
     towns = cur.fetchall()
