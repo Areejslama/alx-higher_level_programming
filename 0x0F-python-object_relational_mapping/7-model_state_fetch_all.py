03f363a0b310447e430583010d8c0c2e12132f43
Areejslama <Areegahmed449@gmail.com>
2024-04-13 18:17:26 -0700

    7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""this script list states"""
+"""This script lists states"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,9 +9,10 @@ from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]))
-    session = sessionmaker(bind=engine)
-    session = session()
-    city = session.query(State).all()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    for town in city:
-        print(town.id, town.name)
+    states = session.query(State).all()
+
+    for state in states:
+        print(state.id, state.name)
