aee6112d8b78aa597fd66d8fe35923194048a113
Areejslama <Areegahmed449@gmail.com>
2024-04-13 17:28:00 -0700

    6

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,12 +8,14 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    city = sys.argv[4]
-    cur.execute("""SELECT cities.id, cities.name FROM
-                cities JOIN states ON states.id=cities.state_id
-                 WHERE states.name = %s
-                 ORDER BY cities.id ASC
-                 """, (city,))
+    state_name = sys.argv[4]
+    cur.execute('''
+        SELECT cities.id, cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    ''', (state_name,))
     towns = cur.fetchall()
     for i in towns:
         print(i)
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script to create base class"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
