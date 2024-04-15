1b5f7b6190de9b344b103c3eaefcf10c0b849367
Areejslama <Areegahmed449@gmail.com>
2024-04-14 17:33:20 -0700

    14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""This script lists states"""
+"""Script to print all City objects from the database"""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from model_city import City
 
-if __name__ = "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@hostname:3306/{}'.format(
-        sys.argv[1], sys.argv[2], sys.agrv[3])
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
 
-        session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City).order_by(City.id)
+
+    for city in cities:
+        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""this script to define city"""
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Integer, column, String
+"""This module defines the City class"""
 
-Base = declarative_base()
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base
 
-class city(Base):
-    """city class to represent table"""
+class City(Base):
+    """City class representing the cities table"""
     __tablename__ = 'cities'
-    id = column(Integer, Primary_key= True)
-    name = column(String(128), nullable=False)
-    state_id = column(Integer, Forign_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State")
