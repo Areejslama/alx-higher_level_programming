5e178b397e21cfe3923339eaa8b8ea2191812642
Areejslama <Areegahmed449@gmail.com>
2024-04-26 16:22:23 -0700

    2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this script to post request"""
+import sys
+import urllib.request
+import urllib.parse
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = urllib.parse.urlencode({'email': 'example@example.com'}.encode('UTF-8'))
+    req = urllib.request.Request(url, data=email, methdo='POST')
+    with urllib.request.urlopen(req) as response:
+        html = respone.read().decode('UTF-8')
+        print(html)
