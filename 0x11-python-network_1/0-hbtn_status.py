64f8f7096427ca39c1b3b1da639d00bfd267ccc8
Areejslama <Areegahmed449@gmail.com>
2024-04-26 13:43:04 -0700

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,7 @@
 """this script to fetch a url"""
 import urllib.request
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        res = response.read()
-        print(res)
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status")
+    as response:
+        html = response.read().decode('utf-8')
+        print(type(html))
