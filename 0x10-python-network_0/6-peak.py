8a976a98b21183c172dc55e2a02227afe299f277
Areejslama <Areegahmed449@gmail.com>
2024-04-25 18:04:11 -0700

    p

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,5 +8,5 @@ def find_peak(list_of_integers):
     for n in range (i):
         if (n == 0 or list_of_integers[n] >= list_of_integers[n - 1]) and ( n == i - 1 or  list_of_integers[n] >=  list_of_integers[n + 1]):
             return list_of_integers[n]
-        return None
+    return None
 
