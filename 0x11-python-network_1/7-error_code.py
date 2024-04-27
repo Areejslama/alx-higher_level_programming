e885aaa302189e052e64520c8118f0a2eafad200
Areejslama <Areegahmed449@gmail.com>
2024-04-26 19:25:50 -0700

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,5 +4,9 @@ import sys
 import requests
 if __name__ == "__main__":
     url = sys.argv[1]
-    r =  requests.get(url)
-    print(r.raise_for_status())
+    try:
+        r =  requests.get(url)
+        r.raise_for_status()
+        pass
+    except requests.RequestException as e:
+        print("Error code:", e)
