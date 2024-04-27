a0439b5ecc586cbd106adf4a7a37776b89159dfa
Areejslama <Areegahmed449@gmail.com>
2024-04-26 19:48:10 -0700

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,7 +4,7 @@ import sys
 import requests
 if __name__ == "__main__":
     url = sys.argv[1]
-    r =  requests.get(url)
+    r = requests.get(url)
     tex = 'Error code: {}'
     code = r.status_code
     if (code >= 400):
