88c1f36e6cbef82793fbd868af29b5f2c6960237
Areejslama <Areegahmed449@gmail.com>
2024-04-26 19:03:15 -0700

    6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this script to post request"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    email = {"email:", email)
+    r = requests.post(url, data=email)
+    print(r.text)
