bff1767b2a6d3a511f67c83d6835bfa2b5a23f3e
Areejslama <Areegahmed449@gmail.com>
2024-04-26 19:09:22 -0700

    6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     e = ({'email': 'email'})
-    r = requests.post(url, data=e)
+    r = requests.get(url, data=e)
     print(r.text)
