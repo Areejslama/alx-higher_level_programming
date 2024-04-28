7dffb790c90bfe1bc05817066b97f5119975cf01
Areejslama <Areegahmed449@gmail.com>
2024-04-27 18:43:30 -0700

    10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,8 +7,6 @@ if __name__ == "__main__":
     password = sys.argv[2]
     url = "https://api.github.com/user"
     auth = (username, password)
-    r =  requests.get(url, auth=auth)
-    if r.status_code == 200:
-        print(r.text)
-    else:
-        print("check your credentials")
+    r = requests.get(url, auth=auth)
+    data = r.json()
+    print(data.get('id'))
