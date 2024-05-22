fa8a64bf5c5f2b2e878d33c905c2d92aae52977b
Areejslama <Areegahmed449@gmail.com>
2024-05-22 16:13:29 -0700

    2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,11 @@
 const request = require('request');
 
 const url = process.argv[2];
-request.get(url, (response, body) => {
-  console.log('code:', response.statusCode);
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
 });
