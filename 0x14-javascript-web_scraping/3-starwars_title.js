b95a3f337625b849646e60f6128f4b4f13a5af85
Areejslama <Areegahmed449@gmail.com>
2024-05-22 16:42:06 -0700

    4

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  }
+  if (response.statusCode === 200) {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  } else {
+    console.error('Error:', response.statusCode);
+  }
+});
